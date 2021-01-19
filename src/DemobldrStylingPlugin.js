88c5c0114c9d8c7c3f18b398d4a17d593a177a9a
jlafer <jlafer@twilio.com>
2021-01-19 07:50:54 -0800

    bumped to 0.0.2

--- src/DemobldrStylingPlugin.js
+++ src/DemobldrStylingPlugin.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import { VERSION } from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
-
-import reducers, { namespace } from './states';
 
 const PLUGIN_NAME = 'DemobldrStylingPlugin';
 
@@ -11,16 +7,8 @@ export default class DemobldrStylingPlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof import('@twilio/flex-ui') }
-   * @param manager { import('@twilio/flex-ui').Manager }
-   */
   init(flex, manager) {
     flex.MainHeader.defaultProps
     .logoUrl = "https://www.mercuryinsurance.com/assets/images/mi-logo-web.svg";
   }
-
 }
